1db4d0f721b05b01059621bc1205d8684b3a60c9
byHYONS <byhyons@gmail.com>
2024-05-22 19:28:28 +0200

    add 1 milestone

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
 // reset
 'use strict'
 
+/* *************************
+        variabili
+************************* */
+
+const todoList = [
+    {
+        text: 'Ripassare cicli, iterazioni e condizioni JavaScript',
+        done: null,
+    },
+    {
+        text: 'Studiare VueJS, leggendo attendamente la documentazione.',
+        done: null,
+    },
+    {
+        text: 'Fare esercitazioni su tutti gli argomenti affrontati.',
+        done: null,
+    },
+];
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+       return {
+         todoList,
+         currentList: null,
+       }
+    },
+    methods: {
+        
+    },
+}).mount('#app');
